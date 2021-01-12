51c1b17f2a878e7599aee8981d8b8b2cdddb5be7
Supporter09 <maivannhatminh2005@gmail.com>
2021-01-12 20:42:56 +0700

    Config

--- Client/router.js
+++ Client/router.js
@@ -5,26 +5,32 @@ var useHash = true; // Defaults to: false
 var hash = '#!'; // Defaults to: '#'
 var router = new Navigo(root, useHash, hash);
 
-
-
-
+router
+  .on('/', function () {
+    // show home page here
+    document.getElementById('app').innerHTML = "<container-box></container-box>";
+  })
+  .resolve();
 
 
 router
   .on('/sign-up', function () {
     // document.getElementById('app').innerHTML ="";
-    document.getElementById('app').innerHTML ="<sign-up></sign-up>";
+    document.getElementById('app').innerHTML = "<sign-up></sign-up>";
     console.log("ban dang o chuc nang dang ki")
   })
   .resolve();
-  
+
 
 router
   .on('/sign-in', function () {
-    document.getElementById('app').innerHTML ="<login-form></login-form>";
+    document.getElementById('app').innerHTML = "<login-form></login-form>";
     console.log("ban dang o chuc nang dang nhap")
   })
   .resolve();
-  
+
+router.on('/films', function () {
+  document.getElementById('app').innerHTML = "<films-component></films-component>";
+})
 
 window.router = router
