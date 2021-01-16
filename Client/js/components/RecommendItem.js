6ea9f7e585c10e739f96e331dd92afba17f05801
Tran Hung <Hungtran-pro@users.noreply.github.com>
2021-01-16 22:53:40 +0700

    upload

--- Client/js/components/RecommendItem.js
+++ Client/js/components/RecommendItem.js
@@ -1,5 +1,5 @@
-const $template = document.createElement('template');
-$template.innerHTML= /*html*/ `
+const $template = document.createElement("template");
+$template.innerHTML = /*html*/ `
     <link rel="stylesheet" href="./css/recommendItem.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
@@ -14,28 +14,42 @@ $template.innerHTML= /*html*/ `
 `;
 
 export default class RecommendItem extends HTMLElement {
-    constructor(recommendedFilmData) {
-        super();
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild($template.content.cloneNode(true));
-        this.$filmImage = this.shadowRoot.getElementById('film-image');
-        this.$filmName = this.shadowRoot.getElementById('film-name');
+  constructor(recommendedFilmData) {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild($template.content.cloneNode(true));
+    this.$card = this.shadowRoot.getElementById("container");
+    this.$filmImage = this.shadowRoot.getElementById("film-image");
+    this.$filmName = this.shadowRoot.getElementById("film-name");
 
-        this.setAttribute('recommended-film', recommendedFilmData);
-    }
+    this.setAttribute("recommended-film", recommendedFilmData);
+  }
 
-    static get observedAttributes () {
-        return ['recommended-film'];
-    }
+  connectedCallback() {
+    this.$card.onclick = () => {
+      console.log("hung");
+      let name = this.$filmName.innerHTML;
+      localStorage.setItem("name", name);
+      // let result =  firebase.firestore().collection('FilmData').where('name', '==', '').get();
+      // console.log(result);
+      // let realdata = getDataFromDocs(result.docs);
+      // console.log(realdata);
+      window.location.href = "#!/livefilm";
+    };
+  }
 
-    attributeChangedCallback(attrName, oldValue, newValue) {
-        if(attrName == "recommended-film"){  
-            let filmData = JSON.parse(newValue);
-            console.log('hung');
-            this.$filmImage.src = filmData.img;
-            this.$filmName.innerHTML = filmData.name;
-        }
+  static get observedAttributes() {
+    return ["recommended-film"];
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName == "recommended-film") {
+      let filmData = JSON.parse(newValue);
+      console.log("hung");
+      this.$filmImage.src = filmData.img;
+      this.$filmName.innerHTML = filmData.name;
     }
+  }
 }
 
-window.customElements.define('recommend-item', RecommendItem);
+window.customElements.define("recommend-item", RecommendItem);
