920dc6b168ce9eb7b17b5ee8879a725363f0b5cc
Tran Hung <Hungtran-pro@users.noreply.github.com>
2021-01-16 22:47:17 +0700

    upload

--- Client/js/components/RecommendContainer.js
+++ Client/js/components/RecommendContainer.js
@@ -3,9 +3,6 @@ import { getDataFromDocs, getDataFromFirebase } from "../utils.js";
 
 const $template = document.createElement("template");
 $template.innerHTML = /*html*/ `
-    <style>
-
-    </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <div id="recommended-films">
     </div>
